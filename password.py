bfba645942f7baacff9a45fab2ee6dcf01ddddb7
E2008-Emre <65651839+E2008-Emre@users.noreply.github.com>
2020-07-08 22:14:40 +0200

    Add files via upload

---
+++ password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+name = input ("Please enter your name : ").title().strip()
+
+if name == "Joseph":
+    print("Hello", name, "\b! The password is W@12")
+else:
+    print("Hello", name, "\b! See you later.")
+
